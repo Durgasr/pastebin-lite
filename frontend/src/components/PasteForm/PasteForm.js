2f6e5b4c2ea1d0f117825dae046cb73799d2b657
Durgasri <challadurgasri2000@gmail.com>
2025-12-31 20:24:32 +0530

    fix styles and backend url

--- frontend/src/components/PasteForm/PasteForm.js
+++ frontend/src/components/PasteForm/PasteForm.js
@@ -1,11 +1,11 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import styles from "./PasteForm.module.css";
 
 const PasteForm = () => {
   const [content, setContent] = useState("");
-  const [ttl, setTtl] = useState("");
-  const [maxViews, setMaxViews] = useState(0);
+  const [ttl, setTtl] = useState(null);
+  const [maxViews, setMaxViews] = useState(null);
   const [url, setUrl] = useState(null);
   const [error, setError] = useState(null);
 
@@ -15,63 +15,72 @@ const PasteForm = () => {
     try {
       const body = {
         content: content,
-        ttl_seconds: ttl,
+        ttl_seconds: ttl > 0 ? parseInt(ttl) : undefined,
         max_views: maxViews > 0 ? parseInt(maxViews) : undefined,
       };
 
-      const res = await axios.post("http://localhost:3700/api/pastes", body, {
-        headers: { "Content-Type": "application/json" },
-      });
+      const res = await axios.post(
+        "https://pastebin-lite-dw5r.onrender.com/api/pastes",
+        body,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
       setUrl(res.data.url);
+      setContent("");
+      setTtl("");
+      setMaxViews("");
     } catch (err) {
       setError(err.response?.data?.err || "Failed to create paste");
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && <p className={styles.error}>{error}</p>}
+    <div className={styles.card}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        {error && <p className={styles.error}>{error}</p>}
 
-      <textarea
-        className={styles.textarea}
-        placeholder="Enter your paste..."
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        required
-      ></textarea>
+        <textarea
+          className={styles.textarea}
+          placeholder="Enter your paste..."
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          required
+        ></textarea>
 
-      <input
-        className={styles.input}
-        type="number"
-        min="1"
-        placeholder="TTL (seconds)"
-        value={ttl}
-        onChange={(e) => setTtl(e.target.value)}
-        required
-      />
+        <input
+          className={styles.formInput}
+          type="number"
+          min="1"
+          placeholder="TTL (seconds)"
+          value={ttl}
+          onChange={(e) => setTtl(e.target.value)}
+        />
 
-      <input
-        className={styles.input}
-        type="number"
-        min="1"
-        placeholder="Max views"
-        value={maxViews}
-        onChange={(e) => setMaxViews(e.target.value)}
-        required
-      />
+        <input
+          className={styles.formInput}
+          type="number"
+          min="1"
+          placeholder="Max views"
+          value={maxViews}
+          onChange={(e) => setMaxViews(e.target.value)}
+        />
 
-      <button type="submit" className={styles.button}>
-        Create Paste
-      </button>
+        <button type="submit" className={styles.createButton}>
+          Create Paste
+        </button>
 
-      {url && (
-        <div className={styles.result}>
-          <p>Shareable URL: </p>
-          <a href={url} target="_blank" rel="noreferrer">
-            {url}
-          </a>
-        </div>
-      )}
-    </form>
+        {url && (
+          <div className={styles.result}>
+            <p>Shareable URL: </p>
+            <a href={url} target="_blank" rel="noreferrer">
+              {url}
+            </a>
+          </div>
+        )}
+      </form>
+    </div>
   );
 };
+
+export default PasteForm;
