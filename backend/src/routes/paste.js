88171e4f4be9d2161b4501c15e77898ed74ba81f
Durgasri <challadurgasri2000@gmail.com>
2025-12-31 19:34:01 +0530

    Removed unwanted code

--- backend/src/routes/paste.js
+++ backend/src/routes/paste.js
@@ -1,13 +1,11 @@
 import express from "express";
-import { createPaste, getPaste, viewPasteHtml } from "../controllers/pastesController.js";
+import { createPaste, getPaste } from "../controllers/pastesController.js";
 
 const router = express.Router();
 
 
 router.route("/api/pastes").post(createPaste);
 router.route("/api/pastes/:id").get(getPaste);
-
-router.route("/p/:id").get(viewPasteHtml);
 
 
 
