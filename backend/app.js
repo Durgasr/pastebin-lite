3352259e5c262ced086503f1f3974c914d72fec2
Durgasri <challadurgasri2000@gmail.com>
2025-12-31 11:53:08 +0530

    import healthzRoutes and dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import healthzRoutes from "./src/routes/healthz.js";
+
+const configPath = path.resolve("config", ".env");
+dotenv.config({ path: configPath });
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api/healthz", healthzRoutes);
 
 export default app;
