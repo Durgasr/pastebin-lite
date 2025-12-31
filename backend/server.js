21f0b5d3fbd154867d84497b3e81639413bfab65
Durgasri <challadurgasri2000@gmail.com>
2025-12-31 13:03:09 +0530

    Add mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+app.listen(port, async (err) => {
+  if (err) {
+    console.log(`server failed with error: ${err}`);
+  } else {
+    await connectDB();
+    console.log(`Server is running on ${port}`);
+  }
 });
